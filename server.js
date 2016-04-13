31957906c432c6da2ec01f0435afd6c453c6d8a8
Mike Mclaren <mike.mclaren@sq1.com>
2016-04-13 18:57:08 -0500

    Initial commit.

---
+++ server.js
@@ -0,0 +1,35 @@
+// Required for async/await, currently.
+require('babel-polyfill');
+
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch(err) {
+    if(err.status === 401) {
+      ctx.body = { message: 'Unauthorized access.' };
+      ctx.status = 401;
+    } else {
+      ctx.body = { message: err.message };
+      ctx.status = err.status || 500;
+    }
+  }
+});
+
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    message: 'Hey, welcome to the Koa v2 starter!'
+  };
+  ctx.status = 200;
+  await next();
+});
+
+app.use(router.routes());
+
+app.listen(process.env.PORT);
+console.log(`Server up and running! On port ${process.env.PORT}!`);
